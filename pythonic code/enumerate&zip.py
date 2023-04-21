910b7d6ffc97093b224ee4fc4392814fdb7ccc7a
Haeun Mok <21700238@handong.edu>
2023-04-21 09:44:52 +0900

    pythonic code update

---
+++ pythonic code/enumerate&zip.py
@@ -0,0 +1,12 @@
+### enumerate : lst에서 element추출할 때 사용. 추출시 elememt 번호를 붙여 추출###
+# dict 타입에서 데이터를 추출할 때 많이 사용. 
+for i, v in enumerate(['abc','def','ghi']):
+    print(i,v)
+    
+    """
+    출력결과 
+
+    0 abc
+    1 def
+    2 ghi
+    """
